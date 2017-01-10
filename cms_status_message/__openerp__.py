c6309520a1548a108031dc1dbb88c16a57cdbed7
Simone Orsi <simahawk@gmail.com>
2017-01-10 18:31:07 +0100

    [add] cms_status_message

---
+++ cms_status_message/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'CMS status message',
+    'summary': """Basic status messages for your CMS system""",
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Camptocamp SA,Odoo Community Association (OCA)',
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'templates/status_message.xml',
+    ],
+}
