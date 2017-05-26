dfaa1b869c04c54af1cfac79293d89db335d9a91
Simone Orsi <simahawk@gmail.com>
2017-05-26 10:39:43 +0200

    Re-license cms_status_message + cms_delete_content to LGPL

--- cms_status_message/__openerp__.py
+++ cms_status_message/__openerp__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2017 Simone Orsi
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'CMS status message',
     'summary': """Basic status messages for your CMS system""",
     'version': '9.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'Camptocamp SA,Odoo Community Association (OCA)',
+    'author': 'Camptocamp,Odoo Community Association (OCA)',
     'depends': [
         'website',
     ],
