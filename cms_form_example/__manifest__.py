0ad1a13d8a59dee4a17e72f7ebdd54323506980f
Simone Orsi <simahawk@gmail.com>
2017-10-02 16:25:47 +0200

    [MIG][10.0] cms_form_example

--- cms_form_example/__manifest__.py
+++ cms_form_example/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'CMS Form example',
     'summary': """
         Basic content type form example""",
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Simone Orsi,Odoo Community Association (OCA)',
     'depends': [
