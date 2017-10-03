f1eba5547db66e69eb0f7fbe6089c932a4809a48
Rémy Taymans <remytaymans@gmail.com>
2017-10-03 11:11:16 +0200

    [ADD] cms_delete_content_example: init addon

---
+++ cms_delete_content_example/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Rémy Taymans
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Cms Delete Content Example',
+    'summary': """
+        Basic content to showcase the CMS delete content""",
+    'version': '10.0.1.0.0',
+    'license': 'LGPL-3',
+    'author': 'Rémy Taymans,Odoo Community Association (OCA)',
+    'depends': [
+        'website',
+        'cms_delete_content',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
