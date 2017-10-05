182ad5809cba7484deaedf57387dea56bc23a8f1
Rémy Taymans <remytaymans@gmail.com>
2017-10-05 10:41:36 +0200

    [ADD] cms_delete_content_example (#29)
    Example module on how to use cms_delete_content to unify cms content deletion UI.

---
+++ cms_delete_content_example/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Rémy Taymans
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Cms Delete Content Example',
+    'summary': """
+        Basic content to showcase the CMS delete content""",
+    'version': '10.0.1.0.0',
+    'license': 'LGPL-3',
+    'author': 'Rémy Taymans,Odoo Community Association (OCA)',
+    'depends': [
+        'website',
+        'cms_delete_content',
+    ],
+    'data': [
+        'data/examples.xml',
+        'data/ir.model.access.csv',
+        'templates/example.xml',
+    ],
+}
