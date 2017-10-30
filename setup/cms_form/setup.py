b7bde8712525032b2655697338a436eda85db6f0
Simone Orsi <simahawk@gmail.com>
2017-10-30 17:48:58 +0100

    [fix] setuptools: include ALL files

--- setup/cms_form/setup.py
+++ setup/cms_form/setup.py
@@ -2,5 +2,7 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={'odoo_version_override': '10.0'},
+    include_package_data=True,
+    package_data={'': ['*', ]}
 )
