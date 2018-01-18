881653fafe52a66820f7941dd556520485962894
Simone Orsi <simahawk@gmail.com>
2018-01-18 18:45:21 +0100

    [MIG][11] cms_status_message_example

--- cms_status_message_example/controllers/main.py
+++ cms_status_message_example/controllers/main.py
@@ -1,19 +1,17 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017
+# Copyright 2017-2018 Camptocamp - Thierry Ducrest
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import http
 
 
 class CMSStatusMsgController(http.Controller):
-    """ CMS status messge testing route"""
+    """CMS status messge testing route."""
 
     @http.route([
         '/status-msg/test',
     ], type='http', auth='public', website=True)
     def cms_form(self, **kw):
-        """ Test the cms status message
-        """
+        """Test the cms status message."""
         msg = http.request.httprequest.args.get('message', 'yes it works')
         msg_title = 'Title'
         for type_ in ('success', 'warning', 'danger', 'info'):
