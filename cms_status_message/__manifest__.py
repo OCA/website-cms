ff0439030fb85c6513cacd06559d5cf3e058f926
Simone Orsi <simahawk@gmail.com>
2018-02-04 15:57:55 +0100

    cms_status_message: improve session handling

--- cms_status_message/__manifest__.py
+++ cms_status_message/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'CMS status message',
     'summary': """Basic status messages for your CMS system""",
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'license': 'LGPL-3',
     'author': 'Camptocamp, Odoo Community Association (OCA)',
     'depends': [
