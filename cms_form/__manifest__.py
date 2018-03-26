286ebc2c6698f0c10f859d6561ec6eef4899091b
Simone Orsi <simahawk@gmail.com>
2018-03-26 15:53:46 +0200

    Bump cms_form 11.0.1.1.0

--- cms_form/__manifest__.py
+++ cms_form/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'CMS Form',
     'summary': """
         Basic content type form""",
-    'version': '11.0.1.0.4',
+    'version': '11.0.1.1.0',
     'license': 'LGPL-3',
     'author': 'Camptocamp, Odoo Community Association (OCA)',
     'depends': [
