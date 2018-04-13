OCA/website-cms

commit 32a662e9027329de43f1c8e7ae0ff9c3d8e341a7

cms_form: move `check_permission` to form

cms_form/models/cms_form_mixin.py
@@ -5,7 +5,7 @@ import inspect
 import json
 from collections import OrderedDict
 
-from odoo import models, tools
+from odoo import models, tools, exceptions
 
 from ..utils import data_merge
 
@@ -135,6 +135,34 @@ class CMSFormMixin(models.AbstractModel):
             if not inspect.ismethod(getattr(form, '_form_' + k)):
                 setattr(form, '_form_' + k, v)
         return form
+
+    def form_check_permission(self):
+        """Check permission on current model and main object if any."""
+        if self.main_object:
+            self._can_edit()
+        else:
+            self._can_create()
+
+    def _can_create(self, raise_exception=True):
+        """Check that current user can create instances of given model."""
+        if self.form_model:
+            return self.form_model.check_access_rights(
+                'create', raise_exception=raise_exception)
+        return True
+
+    def _can_edit(self, raise_exception=True):
+        """Check that current user can edit main object if any."""
+        if not self.main_object:
+            return True
+        try:
+            self.main_object.check_access_rights('write')
+            self.main_object.check_access_rule('write')
+            can = True
+        except exceptions.AccessError:
+            if raise_exception:
+                raise
+            can = False
+        return can
 
     @property
     def form_title(self):

cms_form/controllers/main.py
@@ -3,7 +3,7 @@
 
 import werkzeug
 
-from odoo import http, exceptions, _
+from odoo import http, _
 from odoo.http import request
 
 
@@ -45,24 +45,7 @@ class FormControllerMixin(object):
         })
         return kw
 
-    def _can_create(self, model, raise_exception=True):
-        """Check that current user can create instances of given model."""
-        return request.env[model].check_access_rights(
-            'create', raise_exception=raise_exception)
-
-    def _can_edit(self, main_object, raise_exception=True):
-        """Check that current user can edit given main object."""
-        try:
-            main_object.check_access_rights('write')
-            main_object.check_access_rule('write')
-            can = True
-        except exceptions.AccessError:
-            if raise_exception:
-                raise
-            can = False
-        return can
-
-    def form_model_key(self, model):
+    def form_model_key(self, model, **kw):
         """Return a valid form model."""
         return 'cms.form.' + model
 
@@ -81,13 +64,6 @@ class FormControllerMixin(object):
             )
         return form
 
-    def check_permission(self, model, main_object):
-        """Check permission on current model and main object."""
-        if main_object:
-            self._can_edit(main_object)
-        else:
-            self._can_create(model)
-
     def make_response(self, model, model_id=None, page=0, **kw):
         """Prepare and return form response.
 
@@ -105,11 +81,8 @@ class FormControllerMixin(object):
           it redirects to it.
         * otherwise it just renders the form
         """
-        main_object = request.env[model]
-        if model_id:
-            main_object = request.env[model].browse(model_id)
-        self.check_permission(model, main_object)
-        form = self.get_form(model, main_object=main_object)
+        form = self.get_form(model, model_id=model_id, page=page, **kw)
+        form.form_check_permission()
         # pass only specific extra args, to not pollute form render values
         form.form_process(extra_args={'page': page})
         # search forms do not need these attrs
@@ -118,7 +91,7 @@ class FormControllerMixin(object):
             # anything went fine, redirect to next url
             return werkzeug.utils.redirect(form.form_next_url())
         # render form wrapper
-        values = self.get_render_values(main_object, **kw)
+        values = self.get_render_values(form.main_object, **kw)
         values['form'] = form
         return request.render(
             self.get_template(form, **kw),
@@ -145,9 +118,6 @@ class SearchFormControllerMixin(FormControllerMixin):
 
     def form_model_key(self, model):
         return 'cms.form.search.' + model
-
-    def check_permission(self, model, main_object):
-        pass
 
 
 class CMSSearchFormController(http.Controller, SearchFormControllerMixin):

commit 41f31c9d8cea830d4b944ee6a142170534c4bda3

cms_form: fix search form regression on permission check
In 32a662e I've moved permission check from controller to form
but I missed the bypass for search forms.

cms_form/models/cms_form_mixin.py
@@ -143,6 +143,7 @@ class CMSFormMixin(models.AbstractModel):
             self._can_edit()
         else:
             self._can_create()
+        return True
 
     def _can_create(self, raise_exception=True):
         """Check that current user can create instances of given model."""

cms_form/models/cms_search_form.py
@@ -25,6 +25,10 @@ class CMSFormSearch(models.AbstractModel):
     # declare fields that must be searched w/ multiple values
     _form_search_fields_multi = ()
 
+    def form_check_permission(self):
+        """Just searching, nothing to check here."""
+        return True
+
     def form_update_fields_attributes(self, _fields):
         """No field should be mandatory."""
         super().form_update_fields_attributes(_fields)

cms_form/tests/test_form_search.py
@@ -61,9 +61,10 @@ class TestCMSSearchForm(FormTestCase):
             sorted(expected.mapped('id')),
         )
 
-    def get_search_form(self, data, form_model='cms.form.search.res.partner'):
+    def get_search_form(
+            self, data, form_model='cms.form.search.res.partner', **kw):
         request = fake_request(form_data=data)
-        form = self.get_form(form_model, req=request)
+        form = self.get_form(form_model, req=request, **kw)
         # restrict search results to these ids
         form.test_record_ids = self.expected_partners_ids
         return form
@@ -94,10 +95,15 @@ class TestCMSSearchForm(FormTestCase):
             self.env.ref('base.it').id,
             self.env.ref('base.fr').id,
         ]
-        data = {'country_id':  ','.join(map(str, countries))}
+        data = {'country_id': ','.join(map(str, countries))}
         form = self.get_search_form(
             data, form_model='cms.form.search.res.partner.multicountry')
         form.form_process()
         expected = self.expected_partners.filtered(
             lambda x: x.country_id.id in countries)
         self.assert_results(form, 3, expected)
+
+    def test_search_form_bypass_security_check(self):
+        form = self.get_search_form(
+            {}, sudo_uid=self.env.ref('base.public_user').id)
+        self.assertTrue(form.form_check_permission())