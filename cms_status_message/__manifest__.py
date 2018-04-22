53aa6c6158fd6e0ef68815640545791985efa7a9
Simone Orsi <simahawk@gmail.com>
2018-04-22 14:05:35 +0200

    Bump cms_status_message 11.0.1.1.0

--- cms_status_message/__manifest__.py
+++ cms_status_message/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'CMS status message',
     'summary': """Basic status messages for your CMS system""",
-    'version': '11.0.1.0.1',
+    'version': '11.0.1.1.0',
     'license': 'LGPL-3',
     'author': 'Camptocamp, Odoo Community Association (OCA)',
     'depends': [
