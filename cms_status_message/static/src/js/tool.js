9b0fa2f801aaccd72b0ac8a5d734ef884b0b61d5
Simone Orsi <simahawk@gmail.com>
2018-05-08 08:24:07 +0200

    BACKPORT cms_status_message: improve JS API

--- cms_status_message/static/src/js/tool.js
+++ cms_status_message/static/src/js/tool.js
@@ -38,24 +38,31 @@ odoo.define('cms_status_message.tool', function (require) {
             return WebsiteModel.call('get_status_message', []);
         },
         render_messages: function render_messages(msg, selector) {
-            // defaults
-            var status_message = {
-                'msg': '',
-                'title': null,
-                'type': 'info',
-                'dismissible': true
-            };
-            // inject user values
-            $.extend(status_message, msg);
-            // render it
-            var result = qweb.render(
+            var def = $.Deferred();
+            load_templates([
                 'cms_status_message.message_listing_wrapper',
-                {status_message: [status_message]}
-            );
-            if(selector){
-                $(result).prependTo(selector);
-            }
-            return result;
+                'cms_status_message.message_wrapper',
+            ]).done(function () {
+                // defaults
+                var status_message = {
+                    'msg': '',
+                    'title': null,
+                    'type': 'info',
+                    'dismissible': true
+                };
+                // inject user values
+                $.extend(status_message, msg);
+                // render it
+                var result = qweb.render(
+                    'cms_status_message.message_listing_wrapper',
+                    {status_message: [status_message]}
+                );
+                if(selector){
+                    $(result).prependTo(selector);
+                }
+                def.resolve(result);
+            });
+            return def;
         }
     };
 
