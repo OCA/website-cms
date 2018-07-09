OCA/website-cms

commit 56b37ca92745a23a97a72671a3895e05cf78d50a

cms_form: handle hidden input automatically
You can now specify `_form_fields_hidden = ('foo', )`
to get hidden inputs. All fields declared here
will be rendered as `<input type="hidden" />`.

cms_form/models/cms_form_mixin.py
@@ -76,6 +76,8 @@ class CMSFormMixin(models.AbstractModel):
     _form_fields_whitelist = ()
     # exclude these fields
     _form_fields_blacklist = ()
+    # include fields but make them input[type]=hidden
+    _form_fields_hidden = ()
     # group form fields together
     _form_fieldsets = [
         # {
@@ -214,10 +216,21 @@ class CMSFormMixin(models.AbstractModel):
         # queue_job tries to read properties. Be defensive.
         return self.env.get(self._form_model)
 
-    def form_fields(self):
+    def form_fields(self, hidden=None):
+        """Retrieve form fields.
+
+        :param hidden: whether to include or not hidden inputs.
+            Options:
+            * None, default: include all fields, hidden or not
+            * True: include only hidden fields
+            * False: include all fields but those hidden.
+        """
         _fields = self._form_fields()
         # update fields attributes
         self.form_update_fields_attributes(_fields)
+        if hidden is not None:
+            return {k: v for k, v in _fields.items()
+                    if v.get('hidden', False) == hidden}
         return _fields
 
     @tools.cache('self')
@@ -310,6 +323,8 @@ class CMSFormMixin(models.AbstractModel):
         for fname, field in _fields.items():
             if fname in self._form_required_fields:
                 _fields[fname]['required'] = True
+            if fname in self._form_fields_hidden:
+                _fields[fname]['hidden'] = True
             _fields[fname]['widget'] = self.form_get_widget(fname, field)
 
     @property
@@ -319,6 +334,9 @@ class CMSFormMixin(models.AbstractModel):
 
     def form_get_widget_model(self, fname, field):
         """Retrieve widget model name."""
+        if field.get('hidden'):
+            # special case
+            return 'cms.form.widget.hidden'
         widget_model = 'cms.form.widget.char'
         for key in (field['type'], fname):
             model_key = 'cms.form.widget.' + key

cms_form/tests/test_form_base.py
@@ -98,6 +98,23 @@ class TestFormBase(FormTestCase):
         # this one is forced to required in our custom form
         self.assertTrue(fields['country_id']['required'])
 
+    def test_fields_hidden(self):
+        form = self.get_form(
+            'cms.form.res.partner', fields_hidden=('country_id', ))
+        # get all fields (default)
+        fields = form.form_fields()
+        self.assertListEqual(
+            sorted(fields.keys()), ['country_id', 'custom', 'name', ])
+        # country is flagged as hidden
+        self.assertTrue(fields['country_id']['hidden'])
+        # get only visible
+        fields = form.form_fields(hidden=False)
+        self.assertListEqual(sorted(fields.keys()), ['custom', 'name'])
+        # get only hidden
+        fields = form.form_fields(hidden=True)
+        self.assertListEqual(sorted(fields.keys()), ['country_id', ])
+        self.assertTrue(fields['country_id']['hidden'])
+
     def test_get_loader(self):
         form = self.get_form('cms.form.test_fields')
         expected = {}.fromkeys((

cms_form/models/widgets.py
@@ -76,6 +76,12 @@ class CharWidget(models.AbstractModel):
     _w_template = 'cms_form.field_widget_char'
 
 
+class HiddenWidget(models.AbstractModel):
+    _name = 'cms.form.widget.hidden'
+    _inherit = 'cms.form.widget.mixin'
+    _w_template = 'cms_form.field_widget_hidden'
+
+
 class IntegerWidget(models.AbstractModel):
     _name = 'cms.form.widget.integer'
     _inherit = 'cms.form.widget.char'

cms_form/templates/form.xml
@@ -153,8 +153,13 @@ License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
         <input type="hidden" name="csrf_token" t-att-value="form.o_request.csrf_token()"/>
     </t>
 
-    <t t-set="form_fields" t-value="form.form_fields()" />
+    <t t-set="form_fields" t-value="form.form_fields(hidden=False)" />
+    <t t-set="form_hidden_fields" t-value="form.form_fields(hidden=True)" />
     <t t-set="form_fieldsets" t-value="form.form_fieldsets()" />
+
+    <t t-foreach="form_hidden_fields" t-as="hidden_fname">
+      <t t-raw="form_hidden_fields[hidden_fname]['widget'].render()" />
+    </t>
 
     <t t-call="#{ form.form_fields_template }" />
 

cms_form/templates/widgets.xml
@@ -20,6 +20,19 @@ License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 </template>
 
 
+<template id="field_widget_hidden" name="CMS form hidden field">
+
+  <input type="hidden"
+         t-attf-class="#{widget.w_css_klass}"
+         t-att-name="widget.w_fname"
+         t-att-value="widget.w_field_value"
+         t-att-required="widget.w_field['required'] and '1' or None"
+         t-att-data-params='widget.w_data_json()'
+         />
+
+</template>
+
+
 <template id="field_widget_m2o" name="CMS form m2o field widget">
 
   <select t-attf-class="form-control #{widget.w_css_klass}"

commit 9fef1e4e6aaed0eba3d7d5823e5aef22fdcc0025

cms_form: fix regression fields ordering + hidden
When calling `form_fields` w/ hidden=True/False
the order of the fields was not respected anymore.

This a regression from commit 56b37ca

cms_form/models/cms_form_mixin.py
@@ -229,8 +229,12 @@ class CMSFormMixin(models.AbstractModel):
         # update fields attributes
         self.form_update_fields_attributes(_fields)
         if hidden is not None:
-            return {k: v for k, v in _fields.items()
-                    if v.get('hidden', False) == hidden}
+            # make sure ordering is preserved
+            filtered = OrderedDict()
+            for k, v in _fields.items():
+                if v.get('hidden', False) == hidden:
+                    filtered[k] = v
+            return filtered
         return _fields
 
     @tools.cache('self')

cms_form/tests/test_form_base.py
@@ -115,6 +115,22 @@ class TestFormBase(FormTestCase):
         self.assertListEqual(sorted(fields.keys()), ['country_id', ])
         self.assertTrue(fields['country_id']['hidden'])
 
+    def test_fields_hidden_keep_order(self):
+        form = self.get_form(
+            'cms.form.res.partner',
+            fields_hidden=('country_id', ),
+            fields_order=['country_id', 'name', 'custom'])
+        fields = form.form_fields(hidden=False)
+        self.assertListEqual(
+            list(fields.keys()), ['name', 'custom'])
+        form = self.get_form(
+            'cms.form.res.partner',
+            fields_hidden=('country_id', ),
+            fields_order=['country_id', 'custom', 'name'])
+        fields = form.form_fields(hidden=False)
+        self.assertListEqual(
+            list(fields.keys()), ['custom', 'name', ])
+
     def test_get_loader(self):
         form = self.get_form('cms.form.test_fields')
         expected = {}.fromkeys((