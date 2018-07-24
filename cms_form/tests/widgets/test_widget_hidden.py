c755c68edf159df804d4fd9ad7a5dea89bc5eb7a
Simone Orsi <simahawk@gmail.com>
2018-07-24 09:56:48 +0200

    cms_form: input hidden support cast to float
    Now when you use an hidden input on a float field,
    or a selection field w/ float values, you get request value converted to float

--- cms_form/tests/widgets/test_widget_hidden.py
+++ cms_form/tests/widgets/test_widget_hidden.py
@@ -9,11 +9,13 @@ class TestWidgetChar(TestWidgetCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.form = fake_form(
+            # fake defaults
             char_field='abc',
             int_field=5,
             float_field=5.0,
             selection_str_field='1',
             selection_integer_field=2,
+            selection_float_field=4.0,
             many2one_field=10,
         )
 
@@ -56,25 +58,24 @@ class TestWidgetChar(TestWidgetCase):
         for attr_name, attr_value in expected_attrs.items():
             self.assertEqual(node_input[0].attrib[attr_name], attr_value)
 
-    # TODO: support float conversion
-    # def test_widget_float_input_hidden(self):
-    #     """Test float field hidden."""
-    #     w_name, w_field = fake_field('float_field', type='float')
-    #     widget = self.get_widget(w_name, w_field, form=self.form,
-    #                              widget_model='cms.form.widget.hidden')
-    #     node = self.to_xml_node(widget.render())[0]
-    #     node_input = self.find_input_name(node, widget.w_html_fname)
-    #     self.assertEqual(len(node_input), 1)
-    #     expected_attrs = {
-    #         'type': 'hidden',
-    #         'name': 'float_field:float',
-    #         'value': '5.0',
-    #     }
-    #     for attr_name, attr_value in expected_attrs.items():
-    #         self.assertEqual(node_input[0].attrib[attr_name], attr_value)
+    def test_widget_float_input_hidden(self):
+        """Test float field hidden."""
+        w_name, w_field = fake_field('float_field', type='float')
+        widget = self.get_widget(w_name, w_field, form=self.form,
+                                 widget_model='cms.form.widget.hidden')
+        node = self.to_xml_node(widget.render())[0]
+        node_input = self.find_input_name(node, widget.w_html_fname)
+        self.assertEqual(len(node_input), 1)
+        expected_attrs = {
+            'type': 'hidden',
+            'name': 'float_field:float',
+            'value': '5.0',
+        }
+        for attr_name, attr_value in expected_attrs.items():
+            self.assertEqual(node_input[0].attrib[attr_name], attr_value)
 
     def test_widget_selection_string_input_hidden(self):
-        """Test selection field hidden."""
+        """Test selection field hidden with string values."""
         w_name, w_field = fake_field(
             'selection_str_field',
             type='selection',
@@ -93,7 +94,7 @@ class TestWidgetChar(TestWidgetCase):
             self.assertEqual(node_input[0].attrib[attr_name], attr_value)
 
     def test_widget_selection_integer_input_hidden(self):
-        """Test selection field hidden."""
+        """Test selection field hidden with integer values."""
         w_name, w_field = fake_field(
             'selection_integer_field',
             type='selection',
@@ -107,6 +108,25 @@ class TestWidgetChar(TestWidgetCase):
             'type': 'hidden',
             'name': 'selection_integer_field:int',
             'value': '2',
+        }
+        for attr_name, attr_value in expected_attrs.items():
+            self.assertEqual(node_input[0].attrib[attr_name], attr_value)
+
+    def test_widget_selection_float_input_hidden(self):
+        """Test selection field hidden with float values."""
+        w_name, w_field = fake_field(
+            'selection_float_field',
+            type='selection',
+            selection=[(4.0, 'A'), (8.0, 'B')])
+        widget = self.get_widget(w_name, w_field, form=self.form,
+                                 widget_model='cms.form.widget.hidden')
+        node = self.to_xml_node(widget.render())[0]
+        node_input = self.find_input_name(node, widget.w_html_fname)
+        self.assertEqual(len(node_input), 1)
+        expected_attrs = {
+            'type': 'hidden',
+            'name': 'selection_float_field:float',
+            'value': '4.0',
         }
         for attr_name, attr_value in expected_attrs.items():
             self.assertEqual(node_input[0].attrib[attr_name], attr_value)
