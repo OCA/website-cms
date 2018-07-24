d47c3e9a0f5a54dd9e6edd44cf909b9172f74611
Simone Orsi <simahawk@gmail.com>
2018-07-24 09:51:48 +0200

    cms_form: add :float marshaller
    You can now use `$foo:float` as field name to cast value to float.

--- cms_form/tests/test_marshallers.py
+++ cms_form/tests/test_marshallers.py
@@ -45,6 +45,21 @@ class TestMarshallers(unittest.TestCase):
         self.assertEqual(marshalled['c'], 3)
         self.assertEqual(marshalled['d'], 'bad')
 
+    def test_marshal_float(self):
+        data = MultiDict([
+            ('a', '1'),
+            ('b:float', '2'),
+            ('c:float', '3.0'),
+            ('d:float', '4,0'),
+            ('e:float', 'bad'),
+        ])
+        marshalled = marshallers.marshal_request_values(data)
+        self.assertEqual(marshalled['a'], '1')
+        self.assertEqual(marshalled['b'], 2.0)
+        self.assertEqual(marshalled['c'], 3.0)
+        self.assertEqual(marshalled['d'], 4.0)
+        self.assertEqual(marshalled['e'], 'bad')
+
     def test_marshal_dict(self):
         data = MultiDict([
             ('a', '1'),
