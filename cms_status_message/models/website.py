f78d27109703658577de377cb98537abc33f72e1
Simone Orsi <simahawk@gmail.com>
2018-08-06 15:41:51 +0200

    cms_status_message: add autodismiss option

--- cms_status_message/models/website.py
+++ cms_status_message/models/website.py
@@ -20,7 +20,8 @@ class Website(models.Model):
 
     @api.model
     def add_status_message(self, msg, title='', type_='info',
-                           dismissible=True, session=None):
+                           dismissible=True, dismiss_options=None,
+                           session=None):
         """Inject status message in session.
 
         :param msg: the message you want to display.
@@ -47,10 +48,40 @@ class Website(models.Model):
             'type': type_,
             'dismissible': bool(dismissible),
         }
+        if dismissible and not dismiss_options:
+            dismiss_options = self._get_autodismiss_config()
+        status_message['dismiss_options'] = dismiss_options or {}
         if session is None:
             session = request.session
         if session is not None:
             session.setdefault('status_message', []).append(status_message)
+
+    def _get_autodismiss_config(self):
+        """Retrieve configuration for autodismiss.
+
+        You can create `ir.config_parameter` records to customize:
+
+        * turn on/off -> key="cms_status_message.autodismiss", value="1/0"
+        * timeout -> key="cms_status_message.autodismiss_timeout", value="1000"
+          (in milliseconds)
+
+        Defaults: ON + 8 seconds timeout.
+        """
+        params = self.env['ir.config_parameter'].sudo()
+        autodismiss = params.get_param('cms_status_message.autodismiss') or ''
+        if autodismiss:
+            autodismiss = autodismiss.strip() == '1'
+        else:
+            # default ON
+            autodismiss = True
+        timeout = params.get_param(
+            'cms_status_message.autodismiss_timeout') or ''
+        config = {
+            'autodismiss': autodismiss,
+            'autodismissTimeout':
+                int(timeout.strip()) if timeout.strip() else 8000,
+        }
+        return config
 
     @api.model
     def get_status_message(self, session=None):
