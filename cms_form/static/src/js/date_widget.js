6fa6f4914e76d534f6c46b00b36b6c0659f004ad
Simone Orsi <simahawk@gmail.com>
2018-08-07 12:47:34 +0200

    cms_form: date widget JS datepicker options custom
    You can now override all the options of the datepicker via `data`.
    For instance:
    
        def form_get_widget(self, fname, field, **kw):
            """Customize datepicker."""
            if fname == 'date':
                kw['data'] = {
                    'minDate': '2018-01-01'
                }
            return super().form_get_widget(fname, field, **kw)

--- cms_form/static/src/js/date_widget.js
+++ cms_form/static/src/js/date_widget.js
@@ -3,21 +3,31 @@ odoo.define('cms_form.date_widget', function (require) {
 
   require('web.dom_ready');
 
+  // TODO: migrate to snippet animation
   $(document).ready(function () {
     $("input.js_datepicker").each(function () {
       var $input = $(this);
-      $input.datepicker({
-        useSeconds: false,
-        icons: {
-          time: 'fa fa-clock-o',
-          date: 'fa fa-calendar',
-          up: 'fa fa-chevron-up',
-          down: 'fa fa-chevron-down'
-        },
-        dateFormat: $input.data('format')
-      });
-      if ($input.data('params').defaultToday && !$input.val()) {
-        $input.datepicker("setDate", new Date());
+      var options = _.defaults(
+        $input.data('params') || {}, {
+          useSeconds: false,
+          icons: {
+            time: 'fa fa-clock-o',
+            date: 'fa fa-calendar',
+            up: 'fa fa-chevron-up',
+            down: 'fa fa-chevron-down'
+          },
+          dateFormat: $input.data('format')
+        }
+      )
+      $input.datepicker(options);
+      var defaultDate = $input.data('params').defaultDate;
+      if (!defaultDate) {
+        defaultDate = $input.data('params').defaultToday;
+      }
+      if (defaultDate && !$input.val()) {
+        $input.datepicker(
+            "setDate", new Date(defaultDate)
+        );
       }
       $input
         .closest('.input-group')
