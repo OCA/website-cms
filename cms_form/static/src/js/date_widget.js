d36c1964a85fd9efbc63f476c64134c94fff83f2
Simone Orsi <simahawk@gmail.com>
2018-08-07 12:43:07 +0200

    cms_form: fix JS indentation date_widget.js

--- cms_form/static/src/js/date_widget.js
+++ cms_form/static/src/js/date_widget.js
@@ -1,31 +1,31 @@
 odoo.define('cms_form.date_widget', function (require) {
-    'use strict';
+  'use strict';
 
-    require('web.dom_ready');
+  require('web.dom_ready');
 
-    $(document).ready(function () {
-        $("input.js_datepicker").each(function(){
-            var $input = $(this);
-            $input.datepicker({
-                useSeconds: false,
-                icons : {
-                    time: 'fa fa-clock-o',
-                    date: 'fa fa-calendar',
-                    up: 'fa fa-chevron-up',
-                    down: 'fa fa-chevron-down'
-                },
-                dateFormat : $input.data('format')
-            });
-            if ($input.data('params').defaultToday && !$input.val()) {
-              $input.datepicker("setDate", new Date());
-            }
-            $input
-                .closest('.input-group')
-                .find('.js_datepicker_trigger').click(function(){
-                    $input.datepicker('show');
-                });
+  $(document).ready(function () {
+    $("input.js_datepicker").each(function () {
+      var $input = $(this);
+      $input.datepicker({
+        useSeconds: false,
+        icons: {
+          time: 'fa fa-clock-o',
+          date: 'fa fa-calendar',
+          up: 'fa fa-chevron-up',
+          down: 'fa fa-chevron-down'
+        },
+        dateFormat: $input.data('format')
+      });
+      if ($input.data('params').defaultToday && !$input.val()) {
+        $input.datepicker("setDate", new Date());
+      }
+      $input
+        .closest('.input-group')
+        .find('.js_datepicker_trigger').click(function () {
+          $input.datepicker('show');
         });
-
     });
+
+  });
 
 });
