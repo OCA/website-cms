7d91a3ec8416d9d890f631764809cd88293d0442
Simone Orsi <simahawk@gmail.com>
2019-01-12 10:12:16 +0100

    cms_form: widgets test cov 100%

--- cms_form/tests/__init__.py
+++ cms_form/tests/__init__.py
@@ -8,5 +8,13 @@ from . import test_form_wizard
 from . import test_loaders
 from . import test_marshallers
 from .widgets import test_widget_base
-from .widgets import test_widget_char
+from .widgets import test_widget_text
 from .widgets import test_widget_hidden
+from .widgets import test_widget_integer
+from .widgets import test_widget_float
+from .widgets import test_widget_selection
+from .widgets import test_widget_radio
+from .widgets import test_widget_boolean
+from .widgets import test_widget_date
+from .widgets import test_widget_many2one
+
