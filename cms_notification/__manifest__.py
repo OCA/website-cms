8a34c392f99c992acf7898943df685e0d7537936
Simone Orsi <simahawk@gmail.com>
2019-01-18 09:00:41 +0100

    Bump cms_notification 11.0.1.0.3

--- cms_notification/__manifest__.py
+++ cms_notification/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'CMS notification',
     'summary': """Basic notifications management for your CMS system""",
-    'version': '11.0.1.0.2',
+    'version': '11.0.1.0.3',
     'license': 'LGPL-3',
     'author': 'Camptocamp,Odoo Community Association (OCA)',
     'depends': [
