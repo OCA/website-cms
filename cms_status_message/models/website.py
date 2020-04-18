2eb0f197307d7f8542653099cba74235397ec952
Simone Orsi <simahawk@gmail.com>
2020-04-18 16:17:29 +0200

    [REF] cms_status_message: Black python code

--- cms_status_message/models/website.py
+++ cms_status_message/models/website.py
@@ -6,22 +6,28 @@ from odoo.http import request
 
 
 class Website(models.Model):
-    _inherit = 'website'
+    _inherit = "website"
 
     @property
     def default_status_msg_title(self):
         """Some default msg titles."""
         return {
-            'info': _('Info'),
-            'success': _('Success'),
-            'danger': _('Error'),
-            'warning': _('Warning'),
+            "info": _("Info"),
+            "success": _("Success"),
+            "danger": _("Error"),
+            "warning": _("Warning"),
         }
 
     @api.model
-    def add_status_message(self, msg, title='', type_='info',
-                           dismissible=True, dismiss_options=None,
-                           session=None):
+    def add_status_message(
+        self,
+        msg,
+        title="",
+        type_="info",
+        dismissible=True,
+        dismiss_options=None,
+        session=None,
+    ):
         """Inject status message in session.
 
         :param msg: the message you want to display.
@@ -43,18 +49,18 @@ class Website(models.Model):
         if title is not None:
             title = title or self.default_status_msg_title.get(type_)
         status_message = {
-            'msg': msg,
-            'title': title,
-            'type': type_,
-            'dismissible': bool(dismissible),
+            "msg": msg,
+            "title": title,
+            "type": type_,
+            "dismissible": bool(dismissible),
         }
         if dismissible and not dismiss_options:
             dismiss_options = self._get_autodismiss_config()
-        status_message['dismiss_options'] = dismiss_options or {}
+        status_message["dismiss_options"] = dismiss_options or {}
         if session is None:
             session = request.session
         if session is not None:
-            session.setdefault('status_message', []).append(status_message)
+            session.setdefault("status_message", []).append(status_message)
 
     def _get_autodismiss_config(self):
         """Retrieve configuration for autodismiss.
@@ -67,19 +73,21 @@ class Website(models.Model):
 
         Defaults: ON + 8 seconds timeout.
         """
-        params = self.env['ir.config_parameter'].sudo()
-        autodismiss = params.get_param('cms_status_message.autodismiss') or ''
+        params = self.env["ir.config_parameter"].sudo()
+        autodismiss = params.get_param("cms_status_message.autodismiss") or ""
         if autodismiss:
-            autodismiss = autodismiss.strip() == '1'
+            autodismiss = autodismiss.strip() == "1"
         else:
             # default ON
             autodismiss = True
-        timeout = params.get_param(
-            'cms_status_message.autodismiss_timeout') or ''
+        timeout = (
+            params.get_param("cms_status_message.autodismiss_timeout") or ""
+        )
         config = {
-            'autodismiss': autodismiss,
-            'autodismissTimeout':
-                int(timeout.strip()) if timeout.strip() else 8000,
+            "autodismiss": autodismiss,
+            "autodismissTimeout": int(timeout.strip())
+            if timeout.strip()
+            else 8000,
         }
         return config
 
@@ -94,5 +102,5 @@ class Website(models.Model):
         """
         session = session or request.session
         if session:
-            return session.pop('status_message', [])
+            return session.pop("status_message", [])
         return []
--- cms_status_message/tests/test_message.py
+++ cms_status_message/tests/test_message.py
@@ -11,23 +11,22 @@ import mock
 _logger = logging.getLogger(__name__)
 
 
-IMPORT = 'odoo.addons.cms_status_message.models.website'
+IMPORT = "odoo.addons.cms_status_message.models.website"
 
 
 class HTMLCase(HttpCase):
-
     def setUp(self):
         super().setUp()
-        self.authenticate('admin', 'admin')
-        self.website = self.env['website'].browse(1)
+        self.authenticate("admin", "admin")
+        self.website = self.env["website"].browse(1)
 
     @contextmanager
     def mock_assets(self):
         """Mocks some stuff like request."""
-        with mock.patch('%s.request' % IMPORT) as request:
+        with mock.patch("%s.request" % IMPORT) as request:
             request.session = self.session
             yield {
-                'request': request,
+                "request": request,
             }
 
     def html_get_doc(self, url):
@@ -36,104 +35,112 @@ class HTMLCase(HttpCase):
 
     def test_message_add_message(self):
         # we can add one or more messages
-        self.website.add_status_message('well done!', session=self.session)
-        self.website.add_status_message('once again!', session=self.session)
-        self.assertTrue('status_message' in self.session)
+        self.website.add_status_message("well done!", session=self.session)
+        self.website.add_status_message("once again!", session=self.session)
+        self.assertTrue("status_message" in self.session)
         msgs = self.website.get_status_message(session=self.session)
-        self.assertEqual(msgs[0]['type'], 'info')
-        self.assertEqual(msgs[0]['title'], 'Info')
-        self.assertEqual(msgs[0]['msg'], 'well done!')
-        self.assertEqual(msgs[1]['type'], 'info')
-        self.assertEqual(msgs[1]['title'], 'Info')
-        self.assertEqual(msgs[1]['msg'], 'once again!')
+        self.assertEqual(msgs[0]["type"], "info")
+        self.assertEqual(msgs[0]["title"], "Info")
+        self.assertEqual(msgs[0]["msg"], "well done!")
+        self.assertEqual(msgs[1]["type"], "info")
+        self.assertEqual(msgs[1]["title"], "Info")
+        self.assertEqual(msgs[1]["msg"], "once again!")
 
     def test_message_add_message_extra_args(self):
         # pass a type
         self.website.add_status_message(
-            'watch out!', type_='warning', session=self.session)
+            "watch out!", type_="warning", session=self.session
+        )
         msg = self.website.get_status_message(session=self.session)[0]
-        self.assertEqual(msg['type'], 'warning')
-        self.assertEqual(msg['title'], 'Warning')
-        self.assertEqual(msg['msg'], 'watch out!')
+        self.assertEqual(msg["type"], "warning")
+        self.assertEqual(msg["title"], "Warning")
+        self.assertEqual(msg["msg"], "watch out!")
         # danger, no title
         self.website.add_status_message(
-            'oh no!', title=None, type_='danger', session=self.session)
+            "oh no!", title=None, type_="danger", session=self.session
+        )
         msg = self.website.get_status_message(session=self.session)[0]
-        self.assertEqual(msg['type'], 'danger')
-        self.assertFalse(msg['title'])
-        self.assertEqual(msg['msg'], 'oh no!')
+        self.assertEqual(msg["type"], "danger")
+        self.assertFalse(msg["title"])
+        self.assertEqual(msg["msg"], "oh no!")
         # dismissible
         self.website.add_status_message(
-            'you cannot remove me!', dismissible=False, session=self.session)
+            "you cannot remove me!", dismissible=False, session=self.session
+        )
         msg = self.website.get_status_message(session=self.session)[0]
-        self.assertEqual(msg['dismissible'], False)
-        self.assertEqual(msg['msg'], 'you cannot remove me!')
+        self.assertEqual(msg["dismissible"], False)
+        self.assertEqual(msg["msg"], "you cannot remove me!")
 
     def test_message_render(self):
         with self.mock_assets() as assets:
-            session = assets['request'].session
-            html_ = self.html_get_doc('/')
-            self.assertFalse(html_.find_class('alert'))
-            self.website.add_status_message('oh yeah!', session=session)
-            html_ = self.html_get_doc('/')
-            msg = html_.find_class('alert')[0]
-            self.assertEqual(msg.attrib['role'], 'alert')
-            self.assertEqual(msg.attrib['class'],
-                             'alert alert-info alert-dismissible')
+            session = assets["request"].session
+            html_ = self.html_get_doc("/")
+            self.assertFalse(html_.find_class("alert"))
+            self.website.add_status_message("oh yeah!", session=session)
+            html_ = self.html_get_doc("/")
+            msg = html_.find_class("alert")[0]
+            self.assertEqual(msg.attrib["role"], "alert")
             self.assertEqual(
-                msg.find_class('msg')[0].text.strip(), 'oh yeah!')
+                msg.attrib["class"], "alert alert-info alert-dismissible"
+            )
+            self.assertEqual(msg.find_class("msg")[0].text.strip(), "oh yeah!")
             self.assertEqual(
-                msg.find_class('title')[0].text_content().strip(), 'Info')
+                msg.find_class("title")[0].text_content().strip(), "Info"
+            )
 
     def test_message_render_types(self):
         with self.mock_assets() as assets:
-            session = assets['request'].session
-            html_ = self.html_get_doc('/')
-            self.assertFalse(html_.find_class('alert'))
+            session = assets["request"].session
+            html_ = self.html_get_doc("/")
+            self.assertFalse(html_.find_class("alert"))
 
             expected = [
-                ('watch out!', 'warning', 'Warning'),
-                ('oh no!', 'danger', 'Error'),
-                ('foo!', 'custom', 'Custom'),
+                ("watch out!", "warning", "Warning"),
+                ("oh no!", "danger", "Error"),
+                ("foo!", "custom", "Custom"),
             ]
             for msg, type_, title in expected:
                 self.website.add_status_message(
-                    msg, session=session, type_=type_, title=title)
-            html_ = self.html_get_doc('/')
-            msgs = html_.find_class('alert')
+                    msg, session=session, type_=type_, title=title
+                )
+            html_ = self.html_get_doc("/")
+            msgs = html_.find_class("alert")
 
             for i, (msg, type_, title) in enumerate(expected):
                 el = msgs[i]
-                self.assertEqual(el.attrib['role'], 'alert')
-                klass = 'alert alert-{} alert-dismissible'.format(type_)
-                self.assertEqual(el.attrib['class'], klass)
+                self.assertEqual(el.attrib["role"], "alert")
+                klass = "alert alert-{} alert-dismissible".format(type_)
+                self.assertEqual(el.attrib["class"], klass)
+                self.assertEqual(el.find_class("msg")[0].text.strip(), msg)
                 self.assertEqual(
-                    el.find_class('msg')[0].text.strip(), msg)
-                self.assertEqual(
-                    el.find_class('title')[0].text_content().strip(), title)
+                    el.find_class("title")[0].text_content().strip(), title
+                )
 
     def test_message_render_no_title(self):
         with self.mock_assets() as assets:
-            session = assets['request'].session
-            html_ = self.html_get_doc('/')
-            self.assertFalse(html_.find_class('alert'))
+            session = assets["request"].session
+            html_ = self.html_get_doc("/")
+            self.assertFalse(html_.find_class("alert"))
             # no title
             self.website.add_status_message(
-                'no title pls!', session=session, title=None)
-            html_ = self.html_get_doc('/')
-            msg = html_.find_class('alert')[0]
+                "no title pls!", session=session, title=None
+            )
+            html_ = self.html_get_doc("/")
+            msg = html_.find_class("alert")[0]
             self.assertEqual(
-                msg.find_class('msg')[0].text.strip(), 'no title pls!')
-            self.assertFalse(msg.find_class('title'))
+                msg.find_class("msg")[0].text.strip(), "no title pls!"
+            )
+            self.assertFalse(msg.find_class("title"))
 
     def test_message_render_dismissible(self):
         with self.mock_assets() as assets:
-            session = assets['request'].session
-            html_ = self.html_get_doc('/')
-            self.assertFalse(html_.find_class('alert'))
+            session = assets["request"].session
+            html_ = self.html_get_doc("/")
+            self.assertFalse(html_.find_class("alert"))
             # no title
             self.website.add_status_message(
-                'I stay here forever!', session=session, dismissible=0)
-            html_ = self.html_get_doc('/')
-            msg = html_.find_class('alert')[0]
-            self.assertFalse('alert-dismissible' in msg.attrib['class'])
+                "I stay here forever!", session=session, dismissible=0
+            )
+            html_ = self.html_get_doc("/")
+            msg = html_.find_class("alert")[0]
+            self.assertFalse("alert-dismissible" in msg.attrib["class"])
