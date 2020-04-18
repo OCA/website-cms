cf75040787579eff90c00e6bb759c65f0991ace1
Simone Orsi <simahawk@gmail.com>
2020-04-18 16:17:29 +0200

    [REF] cms_status_message: Black python code

--- cms_status_message/__manifest__.py
+++ cms_status_message/__manifest__.py
@@ -2,18 +2,16 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'CMS status message',
-    'summary': """Basic status messages for your CMS system""",
-    'version': '11.0.1.3.0',
-    'license': 'LGPL-3',
-    'author': 'Camptocamp, Odoo Community Association (OCA)',
-    'depends': [
-        'website',
+    "name": "CMS status message",
+    "summary": """Basic status messages for your CMS system""",
+    "version": "11.0.1.3.0",
+    "license": "LGPL-3",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "depends": ["website",],
+    "data": [
+        "templates/assets.xml",
+        "templates/status_message.xml",
+        "templates/display_test.xml",
     ],
-    'data': [
-        'templates/assets.xml',
-        'templates/status_message.xml',
-        'templates/display_test.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
--- cms_status_message/controllers/main.py
+++ cms_status_message/controllers/main.py
@@ -7,16 +7,21 @@ from odoo import http
 class CMSStatusMsgTest(http.Controller):
     """CMS status messge testing routes."""
 
-    display_test_template = 'cms_status_message.display_test'
+    display_test_template = "cms_status_message.display_test"
 
-    @http.route([
-        '/cms/status-message/display-test',
-    ], type='http', auth='public', website=True, sitemap=False)
+    @http.route(
+        ["/cms/status-message/display-test",],
+        type="http",
+        auth="public",
+        website=True,
+        sitemap=False,
+    )
     def display_test(self, **kw):  # pragma: no cover
         """Test the cms status messages."""
-        msg = http.request.httprequest.args.get('message', 'yes it works')
-        msg_title = 'Title'
-        for type_ in ('success', 'warning', 'danger', 'info'):
+        msg = http.request.httprequest.args.get("message", "yes it works")
+        msg_title = "Title"
+        for type_ in ("success", "warning", "danger", "info"):
             http.request.website.add_status_message(
-                msg, type_=type_, title=msg_title)
+                msg, type_=type_, title=msg_title
+            )
         return http.request.render(self.display_test_template)
