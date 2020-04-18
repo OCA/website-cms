d587205b695d0f47c59c805b95e47a0b664d3a17
Simone Orsi <simahawk@gmail.com>
2020-04-18 16:16:14 +0200

    [REF] cms_info: Black python code

--- cms_info/__manifest__.py
+++ cms_info/__manifest__.py
@@ -3,16 +3,12 @@
 
 {
     "name": "CMS info",
-    "summary":
-        """A set of basic information needed on all published records.""",
+    "summary": """A set of basic information needed on all published records.""",
     "version": "11.0.1.0.2",
     "category": "Website",
     "website": "https://github.com/OCA/website-cms",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "installable": True,
-    "depends": [
-        'website',
-        'cms_delete_content',
-    ],
+    "depends": ["website", "cms_delete_content",],
 }
--- cms_info/tests/test_info_mixin.py
+++ cms_info/tests/test_info_mixin.py
@@ -9,22 +9,27 @@ class TestInfoMixin(test_common.SavepointCase):
     post_install = True
     at_install = False
 
-    TEST_MODELS_KLASSES = [FakeModel, ]
+    TEST_MODELS_KLASSES = [
+        FakeModel,
+    ]
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         for kls in cls.TEST_MODELS_KLASSES:
             kls._test_setup_model(cls.env)
-        cls.record = cls.env[FakeModel._name].create({'name': 'Foo'})
-        user_model = cls.env['res.users'].with_context(
-            tracking_disable=True, no_reset_password=True)
-        cls.user1 = user_model.create({
-            'name': 'User 1',
-            'login': 'user1',
-            'email': 'user1@email.com',
-            'groups_id': [(6, 0, [cls.env.ref('base.group_portal').id])]
-        })
+        cls.record = cls.env[FakeModel._name].create({"name": "Foo"})
+        user_model = cls.env["res.users"].with_context(
+            tracking_disable=True, no_reset_password=True
+        )
+        cls.user1 = user_model.create(
+            {
+                "name": "User 1",
+                "login": "user1",
+                "email": "user1@email.com",
+                "groups_id": [(6, 0, [cls.env.ref("base.group_portal").id])],
+            }
+        )
 
     @classmethod
     def tearDownClass(cls):
@@ -33,19 +38,20 @@ class TestInfoMixin(test_common.SavepointCase):
         super().tearDownClass()
 
     def test_create_url(self):
-        self.assertEqual(self.record.cms_create_url, '/cms/create/fake.model')
+        self.assertEqual(self.record.cms_create_url, "/cms/create/fake.model")
 
     def test_search_url(self):
-        self.assertEqual(self.record.cms_search_url, '/cms/search/fake.model')
+        self.assertEqual(self.record.cms_search_url, "/cms/search/fake.model")
 
     def test_edit_url(self):
         self.assertEqual(
             self.record.cms_edit_url,
-            '/cms/edit/fake.model/%s' % self.record.id)
+            "/cms/edit/fake.model/%s" % self.record.id,
+        )
 
     def test_is_owner(self):
         self.record._test_create_ACL(
-            group_id=self.env.ref('base.group_portal').id
+            group_id=self.env.ref("base.group_portal").id
         )
         self.assertTrue(self.record.cms_is_owner())
         self.assertFalse(self.record.sudo(self.user1).cms_is_owner())
@@ -58,8 +64,7 @@ class TestInfoMixin(test_common.SavepointCase):
 
     def test_can_edit(self):
         self.record._test_create_ACL(
-            group_id=self.env.ref('base.group_portal').id,
-            perm_write=False,
+            group_id=self.env.ref("base.group_portal").id, perm_write=False,
         )
         self.assertTrue(self.record.cms_can_edit())
         self.assertFalse(self.record.sudo(self.user1).cms_can_edit())
@@ -67,15 +72,14 @@ class TestInfoMixin(test_common.SavepointCase):
     def test_can_publish(self):
         # this permission by default is just a proxy to "can edit"
         self.record._test_create_ACL(
-            group_id=self.env.ref('base.group_portal').id,
-            perm_write=False,
+            group_id=self.env.ref("base.group_portal").id, perm_write=False,
         )
         self.assertTrue(self.record.cms_can_publish())
         self.assertFalse(self.record.sudo(self.user1).cms_can_publish())
 
     def test_can_delete(self):
         self.record._test_create_ACL(
-            group_id=self.env.ref('base.group_portal').id,
+            group_id=self.env.ref("base.group_portal").id,
             perm_write=False,
             perm_unlink=False,
         )
@@ -85,27 +89,27 @@ class TestInfoMixin(test_common.SavepointCase):
     def test_info_on_record(self):
         info = self.record.cms_info()
         keys = (
-            'is_owner',
-            'can_edit',
-            'can_create',
-            'can_publish',
-            'can_delete',
-            'create_url',
-            'edit_url',
-            'delete_url',
+            "is_owner",
+            "can_edit",
+            "can_create",
+            "can_publish",
+            "can_delete",
+            "create_url",
+            "edit_url",
+            "delete_url",
         )
         self.assertEqual(sorted(keys), sorted(info.keys()))
 
     def test_info_on_model(self):
         info = self.record.browse().cms_info()
         keys = (
-            'is_owner',
-            'can_edit',
-            'can_create',
-            'can_publish',
-            'can_delete',
-            'create_url',
-            'edit_url',
-            'delete_url',
+            "is_owner",
+            "can_edit",
+            "can_create",
+            "can_publish",
+            "can_delete",
+            "create_url",
+            "edit_url",
+            "delete_url",
         )
         self.assertEqual(sorted(keys), sorted(info.keys()))
