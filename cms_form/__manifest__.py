872d96c7d809b8148ad266a8d70135e11f3535ce
Simone Orsi <simahawk@gmail.com>
2021-05-16 21:53:36 +0200

    cms_form: jump to v13, make uninstallable

--- cms_form/__manifest__.py
+++ cms_form/__manifest__.py
@@ -2,22 +2,18 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'CMS Form',
-    'summary': """
+    "name": "CMS Form",
+    "summary": """
         Basic content type form""",
-    'version': '11.0.1.7.2',
-    'license': 'LGPL-3',
-    'author': 'Camptocamp, Odoo Community Association (OCA)',
-    'depends': [
-        'website',
-        'cms_info',
-        'cms_status_message',
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "depends": ["website", "cms_info", "cms_status_message"],
+    "data": [
+        "security/cms_form.xml",
+        "templates/assets.xml",
+        "templates/form.xml",
+        "templates/widgets.xml",
     ],
-    'data': [
-        'security/cms_form.xml',
-        'templates/assets.xml',
-        'templates/form.xml',
-        'templates/widgets.xml',
-    ],
-    'installable': True,
+    "installable": False,
 }
